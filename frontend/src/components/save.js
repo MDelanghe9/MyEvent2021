556e04888b1d9b113d7dcccf34c68d40712f90bd
Al <alison.denis@epitech.eu>
2021-09-14 15:23:29 +0200

    add Home page css

--- frontend/src/components/save.js
+++ frontend/src/components/save.js
@@ -1,41 +1,37 @@
-
 //change for multiple type tage filter
 const filtreTag = (e) => {
-    if (e.target.value !== "null") {
-      if (filtredEventsActive == false) {
-        const arrayTempo = []
-        events.forEach((event) => {
-          if (event.fields.tags) {
-            arrayTempo.push(event);
-          }
-        });
-     //   console.log(arrayTempo)
-        const result = arrayTempo.filter((event, i) => 
-            event.fields.tags.includes(e.target.value)
-        );
-        console.log(result);
-        setEventsFiltred(result);
-        setFiltredEventsActive(true);
-      }else{
-          const arrayTempo = []
-          eventsFiltred.forEach((event) => {
-            if (event.fields.tags) {
-              arrayTempo.push(event);
-            }
-          });
-          const result = arrayTempo.filter((event) => 
-              event.fields.tags.includes(e.target.value)
-          );
-          console.log(result);
-          setEventsFiltred(result);
-          setFiltredEventsActive(true);
+  if (e.target.value !== "null") {
+    if (filtredEventsActive == false) {
+      const arrayTempo = [];
+      events.forEach((event) => {
+        if (event.fields.tags) {
+          arrayTempo.push(event);
         }
-    }else{
-      // retirer le liftre tag
-      // sans changer les autres filtre actif
-      // setFiltredEventsActive(false)
+      });
+      //   console.log(arrayTempo)
+      const result = arrayTempo.filter((event, i) =>
+        event.fields.tags.includes(e.target.value)
+      );
+      console.log(result);
+      setEventsFiltred(result);
+      setFiltredEventsActive(true);
+    } else {
+      const arrayTempo = [];
+      eventsFiltred.forEach((event) => {
+        if (event.fields.tags) {
+          arrayTempo.push(event);
+        }
+      });
+      const result = arrayTempo.filter((event) =>
+        event.fields.tags.includes(e.target.value)
+      );
+      console.log(result);
+      setEventsFiltred(result);
+      setFiltredEventsActive(true);
     }
-    
-  
-  
-    }
+  } else {
+    // retirer le liftre tag
+    // sans changer les autres filtre actif
+    // setFiltredEventsActive(false)
+  }
+};
